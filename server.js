3930a6435864e568e000a75950f625bf3c2f64d5
krishbhutani <krishbhutani7417@gmail.com>
2024-08-19 00:43:52 +0530

    Version 4

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express')      //Importing Express
 const mongoose = require('mongoose')    //Importing Mongoose
 const db = require('./db');             //Importing Database
 require('dotenv').config();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const app = express()    //MAking an Application
 const port = process.env.PORT || 3000       //Taking a port
+
+//Middle Ware
+const logRequest = (req,res,next)=>{
+    console.log(`[${new Date().toLocaleString()}] Request Made to: ${req.originalUrl}`);
+    next();    //Move to next step , it act like call back function
+}
+app.use(logRequest);
+
 
 //To convert json to object 
 const bodyParser = require('body-parser');
